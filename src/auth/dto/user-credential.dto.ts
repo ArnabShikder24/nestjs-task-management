7a2d6837150925c2787f370bb04eb0f333d522e5
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-11-26 13:28:09 +0600

    password velidation

--- src/auth/dto/user-credential.dto.ts
+++ src/auth/dto/user-credential.dto.ts
@@ -1,4 +1,15 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+  @IsString()
+  @MinLength(6)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: "it should be One Uppercase One Lower case one numbe and one special Char",
+  })
   password: string;
 }
