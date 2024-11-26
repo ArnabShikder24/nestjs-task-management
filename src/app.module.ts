4833e238665f4c90889bf6b5e3183c3630ab8cd8
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-11-26 12:13:22 +0600

    auth module service controller added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './tasks/task.Entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,10 +13,10 @@ import { Task } from './tasks/task.Entity';
       username: "postgres",
       password: "1234",
       database: "task-management",
-      entities: [Task],
       autoLoadEntities: true,
       synchronize: true
-    })
+    }),
+    AuthModule
   ]
 })
 export class AppModule {}
