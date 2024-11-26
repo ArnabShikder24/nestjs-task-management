4f23f7c8d3241c02fdea1c1c0791b8f62a066cca
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-11-26 13:04:19 +0600

    auth api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialDto } from './dto/user-credential.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) { }
+  
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = await this.userRepository.create({ username, password });
+    await this.userRepository.save(user);
+  }
+}
